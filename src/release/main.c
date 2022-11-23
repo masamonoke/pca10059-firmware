95ea644076af1e3358835dd892ff7cb08ac3bb01
masamonoke <m.rudakov@yahoo.com>
2022-11-23 15:54:39 +0300

    Refactor

--- src/release/main.c
+++ src/release/main.c
@@ -4,47 +4,32 @@
 #include "module/io/led/led_soft_pwm.h"
 #include "module/io/button.h"
 #include <math.h>
-#include "nrf_nvmc.h"
 #include "nrfx_nvmc.h"
 
 #include "main_p.h"
 
 #define PAGE_SIZE 0x00001000
 #define DATA_COUNT 3
+#define ERASED_WORD -1
 
 void write_hsv_to_nvm(uint32_t, uint32_t, uint32_t, uint32_t);
 
 int main(void) {
-    app_timer_init();
-    nordic_usb_logging_init();
-    gpio_utils_init();
-    nrfx_pwm_t pwm_instance = NRFX_PWM_INSTANCE(0);
-    nordic_rgb_pwm_utils_init(pwm_instance);
-    button_init();
-    led_soft_pwm_init();
-
-    button_init_press_check(press_handler);
-    button_init_release_check(release_handler);
-    button_set_n(2);
-    button_init_n_click_check(double_click_handler);
-
-    NRF_LOG_INFO("App start running");
-
-    s_timer_init_();
+    init();
 
     s_current_mode_ = NO_INPUT_MODE;
     s_mode_led_behavior_ = s_mode_led_turn_off_;
 
     uint32_t app_data_start_addr = BOOTLOADER_ADDRESS - NRF_DFU_APP_DATA_AREA_SIZE;
     uint32_t* h_data = (uint32_t*) app_data_start_addr;
-    uint32_t* s_data = (uint32_t*) (app_data_start_addr + 0x00001000);
-    uint32_t* v_data = (uint32_t*) (app_data_start_addr + 0x00002000);
+    uint32_t* s_data = (uint32_t*) (app_data_start_addr + PAGE_SIZE);
+    uint32_t* v_data = (uint32_t*) (app_data_start_addr + (PAGE_SIZE * 2));
 
     uint16_t initial_hue;
     uint16_t initial_satur;
     uint16_t initial_value;
 
-    if (*h_data == -1 || *s_data == -1 || *v_data == -1) {
+    if (*h_data == ERASED_WORD || *s_data == ERASED_WORD || *v_data == ERASED_WORD) {
         initial_hue = (uint16_t) ceilf(360.f * LAST_ID_DIGITS / 100.f);
         initial_satur = 100;
         initial_value = 100;
@@ -73,7 +58,6 @@ int main(void) {
             NRF_LOG_INFO("Values have been written to NVM");
             NRF_LOG_INFO("%d %d %d", *h_data, *s_data, *v_data);
         }
-
 
         LOG_BACKEND_USB_PROCESS();
         NRF_LOG_PROCESS();
