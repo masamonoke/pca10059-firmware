753887243198f2c673909f36411bc9ede9a08154
masamonoke <masamonoke@yahoo.com>
2022-12-07 23:26:58 +0300

    added converter tests

--- src/test/suit_converter/converter_test.c
+++ src/test/suit_converter/converter_test.c
@@ -30,6 +30,60 @@ TEST(Converter, FromRGBtoHSV) {
     TEST_ASSERT_EQUAL_HEX16(29, hsv.hue);
     TEST_ASSERT_EQUAL_HEX16(90, hsv.saturation);
     TEST_ASSERT_EQUAL_HEX16(91, hsv.value);
+
+    //magenta
+    rgb = converter_get_rgb_data(255, 0, 255);
+    hsv = converter_to_hsv_from_rgb(rgb);
+    TEST_ASSERT_EQUAL_HEX16(300, hsv.hue);
+    TEST_ASSERT_EQUAL_HEX16(100, hsv.saturation);
+    TEST_ASSERT_EQUAL_HEX16(100, hsv.value);
+
+    //cyan
+    rgb = converter_get_rgb_data(0, 255, 255);
+    hsv = converter_to_hsv_from_rgb(rgb);
+    TEST_ASSERT_EQUAL_HEX16(180, hsv.hue);
+    TEST_ASSERT_EQUAL_HEX16(100, hsv.saturation);
+    TEST_ASSERT_EQUAL_HEX16(100, hsv.value);
+
+    //white
+    rgb = converter_get_rgb_data(255, 255, 255);
+    hsv = converter_to_hsv_from_rgb(rgb);
+    TEST_ASSERT_EQUAL_HEX16(0, hsv.hue);
+    TEST_ASSERT_EQUAL_HEX16(0, hsv.saturation);
+    TEST_ASSERT_EQUAL_HEX16(100, hsv.value);
+
+    rgb = converter_get_rgb_data(0, 139, 139);
+    hsv = converter_to_hsv_from_rgb(rgb);
+    TEST_ASSERT_EQUAL_HEX16(180, hsv.hue);
+    TEST_ASSERT_EQUAL_HEX16(100, hsv.saturation);
+    //actually 54.5, but doing ceil
+    TEST_ASSERT_EQUAL_HEX16(55, hsv.value);
+
+    rgb = converter_get_rgb_data(45, 235, 34);
+    hsv = converter_to_hsv_from_rgb(rgb);
+    TEST_ASSERT_EQUAL_HEX16(117, hsv.hue);
+    //actually 85.5
+    TEST_ASSERT_EQUAL_HEX16(86, hsv.saturation);
+    //actually 92.2
+    TEST_ASSERT_EQUAL_HEX16(93, hsv.value);
+
+    rgb = converter_get_rgb_data(255, 255, 34);
+    hsv = converter_to_hsv_from_rgb(rgb);
+    TEST_ASSERT_EQUAL_HEX16(60, hsv.hue);
+    TEST_ASSERT_EQUAL_HEX16(87, hsv.saturation);
+    TEST_ASSERT_EQUAL_HEX16(100, hsv.value);
+
+    rgb = converter_get_rgb_data(186, 255, 234);
+    hsv = converter_to_hsv_from_rgb(rgb);
+    TEST_ASSERT_EQUAL_HEX16(162, hsv.hue);
+    TEST_ASSERT_EQUAL_HEX16(28, hsv.saturation);
+    TEST_ASSERT_EQUAL_HEX16(100, hsv.value);
+
+    rgb = converter_get_rgb_data(255, 255, 0);
+    hsv = converter_to_hsv_from_rgb(rgb);
+    TEST_ASSERT_EQUAL_HEX16(60, hsv.hue);
+    TEST_ASSERT_EQUAL_HEX16(100, hsv.saturation);
+    TEST_ASSERT_EQUAL_HEX16(100, hsv.value);
 }
 
 TEST(Converter, SetUpperBoundsHSV) {
